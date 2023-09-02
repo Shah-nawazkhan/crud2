11b9c5d31a2a53b9d26bd49fd834e48940b08e52
Shahnawaz <shahnawazkhan333@live.com>
2023-09-02 23:41:00 +0500

    Added my project

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ else{
   </div>
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#"><img src="/crud/logo.png" height="28px" alt=""></a>
+    <a class="navbar-brand" href="#"><img src="/crud/img/logo.png" height="28px" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
